81dceedd3744456f1468557a96da53a560f741d2
Valery Louis <louisvalery2022@gmail.com>
2025-01-01 12:27:17 -0500

    disabled payments

--- src/components/PaymentMethod.jsx
+++ src/components/PaymentMethod.jsx
@@ -5,9 +5,10 @@ export default function PaymentMethod({logo, name, qrCode, info1, info2}) {
   return (
     <div className='payment-method white-background black-text rounded-border'>
         <img className='payment-logo' src={logo}></img>
-        <img className='payment-qrcode' src={qrCode}></img>
-        <p>{info1}</p>
-        <p>{info2}</p>
+        {/* <img className='payment-qrcode' src={qrCode}></img> */}
+        {/* <p>{info1}</p>
+        <p>{info2}</p> */}
+        <h1>Not yet set up</h1>
     </div>
   )
 }
