b4f986cbf21cd1914b5c2201dcda0c516a27f7ed
Valery Louis <louisvalery2022@gmail.com>
2025-01-13 13:26:19 -0500

    testing haitian creole version

--- src/data/language.js
+++ src/data/language.js
@@ -1,7 +1,7 @@
 export function getLanguage() {
     let language = localStorage.getItem("language");
     if (language == null) {
-        return "English";
+        return "Creole";
     } else {
         return language;
     }
