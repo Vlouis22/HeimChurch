3282e9271f92a3e96515853202ba9986a300fc36
Valery Louis <louisvalery2022@gmail.com>
2025-02-02 23:59:24 -0500

    removed redundancy

--- src/pages/Womens.jsx
+++ src/pages/Womens.jsx
@@ -5,19 +5,18 @@ import { WomenQuestions } from '../data/questions';
 import Questions from '../components/Questions';
 import { database } from '../data/ChurchDatabase';
 import Footer from '../components/Footer';
+import { data } from 'react-router-dom';
 
 export default function Womens() {
-
-  let language = database
 
   return (
     <>
         <div className="maximum-height">
           <Ministry 
-            name={language.ministries.Womens.name} 
+            name={database.ministries.Womens.name} 
             picture1={picture1} 
-            descriptionTitle1={language.ministries.Womens.descriptionTitle1} 
-            description1={language.ministries.Womens.description1}
+            descriptionTitle1={database.ministries.Womens.descriptionTitle1} 
+            description1={database.ministries.Womens.description1}
             questions={<Questions questions={WomenQuestions}/>}
           />
         </div>
