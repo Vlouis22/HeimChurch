ab9010409346bc914644f655b847581a0dc0194c
Valery Louis <louisvalery2022@gmail.com>
2025-02-18 19:41:04 -0500

    added other option to watch live stream

--- src/components/LiveError.jsx
+++ src/components/LiveError.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import RegularButton from '../components/RegularButton';
-import { database } from '../data/ChurchDatabase.js';
+import React, { useState } from 'react';
 
 
 export default function LiveError() {
+
+  const [isHovered, setIsHovered] = useState(false);
+
+  const youtubeLink  = 'https://www.youtube.com/@heimchurch3540';
+
+  const handleMouseEnter = () => setIsHovered(true);
+  const handleMouseLeave = () => setIsHovered(false);  
+
+  const hoverClass = isHovered ? 'white-background red-corner red-text': 'red-background white-text'
+
+  async function handleClick() {
+    window.open(youtubeLink, '_blank');
+  }
+
   return (
-    <div style={{width: '50%', backgroundColor: 'green'}}>
-        <div>If you're experiencing issues watching the live stream, please visit our YouTube page to watch it there.</div>
+    <div className='live-error'>
+        <div className='live-error-title inter'>Having trouble with the live stream? Watch on our YouTube page.</div>
         <div>
-            <RegularButton color='white' bgcolor='red' text='Youtube' width={135} height={54} to='Calendar'/>
+            <div
+              onClick={handleClick}
+              className={`${hoverClass} rounded-border underlined centered-text button`}
+              style={{ width: '130px', height: '50px' }}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              <div className='centered-text roboto-bold' style={{ fontSize: '23px'}}>
+                Youtube
+              </div>
+            </div>
         </div>
     </div>
   )
